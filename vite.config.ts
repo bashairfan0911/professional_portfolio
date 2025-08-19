4bbb7ba0563236684c7922a3c702a1a110e7e608
bashairfan0911 <bashairfan0911@gmail.com>
2025-08-19 12:42:25 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Modern_portfolio/", // GitHub repo name
   server: {
     host: "::",
     port: 8080,
+    base:"/Modern_portfolio"
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
   },
 }));
